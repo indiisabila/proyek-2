247af73d0f34e3fb2a953f08844ec3b7aa7615b0
indiisabila <indisabila5@gmail.com>
2025-01-06 10:03:04 +0700

    Add files via upload

---
+++ list.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+
+<!-- This code was generated using AnimaApp.com. 
+This code is a high-fidelity prototype.
+Get developer-friendly React or HTML/CSS code for this project at: https://projects.animaapp.com?utm_source=hosted-code 
+10/12/2024 02:02:30 -->
+
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=1440, maximum-scale=1.0">
+    <link rel="shortcut icon" type="image/png" href="https://animaproject.s3.amazonaws.com/home/favicon.png">
+    <meta name="og:type" content="website">
+    <link rel="stylesheet" href="style.css">
+    <meta name="twitter:card" content="photo">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <script src="https://animaapp.s3.amazonaws.com/launchpad-static/launchpad.js"></script>
+    <script id="anima-load-script" src="load.js"></script>
+    <script id="anima-hotspots-script" src="hotspots.js"></script>
+    <script id="anima-overrides-script" src="overrides.js"></script>
+    <script src="https://animaapp.s3.amazonaws.com/js/timeline.js"></script>
+    <style>
+
+    </style>
+</head>
+
+<body style="margin: 0;background: #ffffff;"><input type="hidden" id="anPageName" name="page" value="aku-bisa">
+    <div class="container-center-horizontal">
+        <div class="aku-bisa screen " data-id="25:131">
+            <div class="home-dLgTfx" data-id="25:132"><img class="rectangle-3-vkI6KG rectangle-3" data-id="25:133" id="rectangle-3" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" anima-src="https://cdn.animaapp.com/projects/6757021cdec8e05f06cce034/releases/6757038a191b26897c86e1e7/img/rectangle-3.png" alt="Rectangle 3">
+                <div class="home_container-vkI6KG" data-id="25:134">
+                    <h1 class="title-eDASz8" data-id="25:136">Aku Bisa</h1>
+                    <p class="bawa-aku-bisa-untuk-eDASz8" data-id="25:140">Bawa Aku Bisa untuk Kehidupan <br>Anak yang Lebih Baik</p><a href="utama.php?page=daftar_observasi_anak">
+                        <div class="button-eDASz8 button" data-id="270:38">
+                            <div class="daftar-observasi-anak-v2b7lx valign-text-middle worksans-medium-white-15px" data-id="270:39">Daftar Observasi Anak</div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="about-dLgTfx" data-id="25:142" id="about">
+                <div class="rectangle-6-RoJmPw" data-id="25:143"></div>
+                <div class="about__container-RoJmPw" data-id="25:144">
+                    <div class="rectangle-8-6sjVFd" data-id="25:145"></div><img class="rectangle-7-6sjVFd" data-id="25:146" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" anima-src="https://cdn.animaapp.com/projects/6757021cdec8e05f06cce034/releases/6757038a191b26897c86e1e7/img/rectangle-7@2x.png" alt="Rectangle 7">
+                    <div class="about__data-6sjVFd" data-id="25:147">
+                        <div class="tentang-kami-JxOIlz tentang-kami" data-id="25:148"><span class="span0-jHBmee poppins-semi-bold-blueberry-16px">Tentang</span><span class="span1-jHBmee">&nbsp;</span><span class="span2-jHBmee">Kami</span></div>
+                        <p class="selamat-datang-di-website-aku-bisa-JxOIlz valign-text-middle poppins-semi-bold-onyx-36px" data-id="25:149">Selamat Datang di Website Aku Bisa.</p>
+                        <p class="aku-bisa-adalah-sebu-JxOIlz valign-text-middle poppins-medium-onyx-16px" data-id="25:150">Aku Bisa adalah sebuah center yang membantu anak-anak agar mempunyai tumbuh kembang yang optimal, menyiapkan bekal hidup anak untuk menghadapi tantangan masa depan menjadi anak yang mandiri, kreatif, pintar,dan berprestasi dalam perjalanan hidupnya.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="services-dLgTfx" data-id="25:154" id="services">
+                <div class="rectangle-9-zxWYLp" data-id="25:155" id="rectangle-9"></div>
+                <div class="rectangle-10-zxWYLp" data-id="255:2"></div><a href="utama.php?page=artikel5">
+                    <div class="button-zxWYLp button" data-id="105:110">
+                        <div class="rectangle-5-H8FbBx rectangle-5" data-id="105:111"></div>
+                        <div class="group-1-H8FbBx group-1" data-id="105:112">
+                            <div class="gangguan-disaudia-R3ICbf inter-medium-white-16px" data-id="105:113">Gangguan Disaudia</div>
+                        </div>
+                    </div>
+                </a><a href="artikel-9">
+                    <div class="button-vxZEcw button" data-id="105:106">
+                        <div class="rectangle-5-j7wgSo rectangle-5" data-id="105:107"></div>
+                        <div class="group-1-j7wgSo group-1" data-id="105:108">
+                            <div class="gdd-rDpuAQ inter-medium-white-16px" data-id="105:109">GDD</div>
+                        </div>
+                    </div>
+                </a><a href="utama.php?page=artikel4">
+                    <div class="button-P6tAPm button" data-id="105:102">
+                        <div class="rectangle-5-73OH5d rectangle-5" data-id="105:103"></div>
+                        <div class="group-1-73OH5d group-1" data-id="105:104">
+                            <div class="disleksia-Fc85jW inter-medium-white-16px" data-id="105:105">Disleksia</div>
+                        </div>
+                    </div>
+                </a>
+                <div class="button-PajwbX button" data-id="105:98">
+                    <div class="rectangle-5-MxES52 rectangle-5" data-id="105:99"></div><a href="artikel-10">
+                        <div class="group-1-MxES52 group-1" data-id="105:100">
+                            <div class="intelectual-disability-eIV2vn inter-medium-white-16px" data-id="105:101">Intelectual Disability</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="button-9L0Meu button" data-id="105:94">
+                    <div class="rectangle-5-tr16eJ rectangle-5" data-id="105:95"></div>
+                </div><a href="utama.php?page=artikel7">
+                    <div class="button-lfKhf4 button" data-id="105:90"><img class="rectangle-5-2r608A rectangle-5" data-id="105:91" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" anima-src="https://cdn.animaapp.com/projects/6757021cdec8e05f06cce034/releases/6757038a191b26897c86e1e7/img/rectangle-5.svg" alt="Rectangle 5">
+                        <div class="group-1-2r608A group-1" data-id="105:92">
+                            <div class="celebral-palsy-WAd3cv inter-medium-white-16px" data-id="105:93">Celebral Palsy</div>
+                        </div>
+                    </div>
+                </a><a href="artikel-4">
+                    <div class="button-MIb5t4 button" data-id="105:86">
+                        <div class="rectangle-5-Ft7ujZ rectangle-5" data-id="105:87"></div>
+                        <div class="group-1-Ft7ujZ group-1" data-id="105:88">
+                            <div class="terlambat-bicara-69GOwS inter-medium-white-16px" data-id="105:89">Terlambat Bicara</div>
+                        </div>
+                    </div>
+                </a><a href="utama.php?page=artikel1">
+                    <div class="button-uzFjwQ button" data-id="105:81">
+                        <div class="rectangle-5-p4vNZJ rectangle-5" data-id="105:82"></div>
+                        <div class="group-1-p4vNZJ group-1" data-id="105:83">
+                            <div class="down-syndrom-bZMN46 inter-medium-white-16px" data-id="105:84">Down Syndrom</div>
+                        </div>
+                    </div>
+                </a>
+                <div class="melayani-gangguan-anak-zxWYLp valign-text-middle poppins-semi-bold-onyx-36px" data-id="25:157">Melayani Gangguan Anak :</div><a href="utama.php?page=artikel3">
+                    <div class="adhd-zxWYLp valign-text-middle body-text" data-id="27:20">ADHD</div>
+                </a><a href="artikel-6">
+                    <div class="button-nZ8dx8 button" data-id="105:114">
+                        <div class="rectangle-5-rnenVV rectangle-5" data-id="105:115"></div>
+                        <div class="group-1-rnenVV group-1" data-id="105:116">
+                            <div class="gangguan-belajar-XOxzIc inter-medium-white-16px" data-id="105:117">Gangguan Belajar</div>
+                        </div>
+                    </div>
+                </a>
+                <div class="button-Q9xOSU button" data-id="105:76"><a href="utama.php?page=artikel2">
+                        <div class="rectangle-5-dw8tnJ rectangle-5" data-id="105:77"></div>
+                    </a>
+                    <div class="group-1-dw8tnJ group-1" data-id="105:78">
+                        <div class="autis-W1sJXl inter-medium-white-16px" data-id="105:79">Autis</div>
+                    </div>
+                </div>
+            </div>
+            <div class="menu-dLgTfx menu" data-id="25:170" id="menu"><img class="foto-kegiatan-2-fvZxXA foto-kegiatan-2" data-id="205:5" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" anima-src="https://cdn.animaapp.com/projects/6757021cdec8e05f06cce034/releases/6757038a191b26897c86e1e7/img/foto-kegiatan-2-1.png" alt="foto kegiatan 2">
+                <div class="foto-fvZxXA valign-text-middle poppins-semi-bold-onyx-36px" data-id="25:172">Foto</div>
+                <div class="kegiatan-fvZxXA valign-text-middle poppins-semi-bold-onyx-36px" data-id="25:173">Kegiatan</div><img class="foto-kegiatan-1-fvZxXA foto-kegiatan-1" data-id="205:4" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" anima-src="https://cdn.animaapp.com/projects/6757021cdec8e05f06cce034/releases/6757038a191b26897c86e1e7/img/foto-kegiatan-1-1.png" alt="foto kegiatan 1"><img class="foto-kegiatan-3-fvZxXA foto-kegiatan-3" data-id="205:6" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" anima-src="https://cdn.animaapp.com/projects/6757021cdec8e05f06cce034/releases/6757038a191b26897c86e1e7/img/foto-kegiatan-3-1.png" alt="foto kegiatan 3"><img class="foto-kegiatan-1-GF1x0M foto-kegiatan-1" data-id="205:2" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" anima-src="https://cdn.animaapp.com/projects/6757021cdec8e05f06cce034/releases/6757038a191b26897c86e1e7/img/foto-kegiatan-1-2.png" alt="foto kegiatan 1"><img class="foto-kegiatan-4-fvZxXA foto-kegiatan-4" data-id="242:3" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" anima-src="https://cdn.animaapp.com/projects/6757021cdec8e05f06cce034/releases/6757038a191b26897c86e1e7/img/foto-kegiatan-4-1.png" alt="foto kegiatan 4">
+            </div>
+            <div class="ulasan-dLgTfx ulasan" data-id="255:10" id="ulasan"><img class="foto-kegiatan-2-jLmOLf foto-kegiatan-2" data-id="255:11" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" anima-src="https://cdn.animaapp.com/projects/6757021cdec8e05f06cce034/releases/6757038a191b26897c86e1e7/img/foto-kegiatan-2.png" alt="foto kegiatan 2">
+                <div class="ulasan-jLmOLf valign-text-middle ulasan poppins-semi-bold-onyx-36px" data-id="255:13">Ulasan</div><img class="foto-kegiatan-1-jLmOLf foto-kegiatan-1" data-id="255:14" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" anima-src="https://cdn.animaapp.com/projects/6757021cdec8e05f06cce034/releases/6757038a191b26897c86e1e7/img/foto-kegiatan-1.png" alt="foto kegiatan 1"><img class="foto-kegiatan-3-jLmOLf foto-kegiatan-3" data-id="255:15" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" anima-src="https://cdn.animaapp.com/projects/6757021cdec8e05f06cce034/releases/6757038a191b26897c86e1e7/img/foto-kegiatan-3.png" alt="foto kegiatan 3"><img class="foto-kegiatan-4-jLmOLf foto-kegiatan-4" data-id="255:17" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" anima-src="https://cdn.animaapp.com/projects/6757021cdec8e05f06cce034/releases/6757038a191b26897c86e1e7/img/foto-kegiatan-4.png" alt="foto kegiatan 4">
+            </div>
+            <div class="contact-dLgTfx" data-id="25:211" id="contact">
+                <div class="rectangle-13-NQ0hlx" data-id="25:212"></div>
+                <div class="contant__container-NQ0hlx" data-id="25:213">
+                    <div class="contact__data-y9LWpG" data-id="25:214">
+                        <div class="lets-talk-uqXF94 valign-text-middle poppins-semi-bold-blueberry-16px" data-id="25:215">Let’s talk</div>
+                        <div class="kontak-kami-uqXF94 valign-text-middle poppins-semi-bold-onyx-36px" data-id="25:216">Kontak Kami</div>
+                        <p class="mari-mengobrol-dan-b-uqXF94 valign-text-middle poppins-medium-dove-gray-16px" data-id="25:217">Mari mengobrol dan bertanya mengenai Aku Bisa melalui sosial media<br>WhatsApp kami.</p>
+                    </div><a href="http://wa.me/+6285175400177" target="_blank">
+                        <div class="button-y9LWpG button" data-id="25:218">
+                            <div class="rectangle-5-iaGBJ9 rectangle-5" data-id="25:219"></div>
+                            <div class="group-1-iaGBJ9 group-1" data-id="25:220">
+                                <div class="mari-bertanya-vXoa4u inter-medium-white-16px" data-id="25:221">Mari Bertanya</div><img class="instagram-logo-240-1-vXoa4u" data-id="25:222" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" anima-src="https://cdn.animaapp.com/projects/6757021cdec8e05f06cce034/releases/6757038a191b26897c86e1e7/img/instagram-logo-240-1.png" alt="instagram-logo-240 1">
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <footer class="footer-dLgTfx" data-id="25:223">
+                <div class="rectangle-14-6TkI5r" data-id="25:224"></div>
+                <p class="x2024-aku-bisa-all-right-reserved-6TkI5r valign-text-middle poppins-medium-quick-silver-14px" data-id="25:225">© 2024 Aku Bisa. All right reserved</p>
+                <div class="footer__content-6TkI5r footer__content" data-id="25:226">
+                    <div class="rectangle-16-lFlp0h rectangle-16" data-id="25:227"></div>
+                    <div class="layanan-lFlp0h valign-text-middle layanan poppins-bold-onyx-24px" data-id="25:228">Layanan</div><a href="#rectangle-9">
+                        <div class="pelayanan-lFlp0h valign-text-middle poppins-medium-dove-gray-16px" data-id="25:229">Pelayanan</div>
+                    </a>
+                    <div class="menu-lFlp0h valign-text-middle menu poppins-medium-dove-gray-16px" data-id="25:230">Menu</div>
+                </div>
+                <div class="footer__content-tCjF57 footer__content" data-id="25:235">
+                    <div class="rectangle-16-wfbSjI rectangle-16" data-id="25:236"></div>
+                    <div class="alamat-wfbSjI valign-text-middle poppins-bold-onyx-24px" data-id="25:237">Alamat</div>
+                    <div class="ruko-citra-garden-square-wfbSjI valign-text-middle poppins-medium-dove-gray-16px" data-id="25:238">Ruko Citra Garden Square</div>
+                    <p class="jl-purwodadi-no-3-blok-r-01-wfbSjI valign-text-middle poppins-medium-dove-gray-16px" data-id="25:239">Jl. Purwodadi No. 3 Blok R 01</p>
+                    <div class="kota-pekanbaru-wfbSjI valign-text-middle poppins-medium-dove-gray-16px" data-id="25:240">Kota Pekanbaru</div>
+                    <div class="x0811-922-7878-wfbSjI valign-text-middle poppins-medium-dove-gray-16px" data-id="25:241">0811-922-7878</div>
+                    <div class="akubisagmailcom-wfbSjI valign-text-middle poppins-medium-dove-gray-16px" data-id="23:45">akubisa@gmail.com</div>
+                </div>
+                <div class="footer__content-xkxY0h footer__content" data-id="25:242">
+                    <div class="rectangle-15-5CNwq9" data-id="25:243"></div>
+                    <div class="aku-bisa-5CNwq9 valign-text-middle aku-bisa-1 poppins-bold-blueberry-20px" data-id="25:244">Aku Bisa</div>
+                    <div class="therapy-center-terbaik-5CNwq9 valign-text-middle poppins-medium-dove-gray-14px" data-id="25:245">Therapy Center Terbaik!</div><a href="https://www.instagram.com/akubisa.terapianak/" target="_blank"><img class="instagram-logo-240-1-1-5CNwq9" data-id="25:246" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" anima-src="https://cdn.animaapp.com/projects/6757021cdec8e05f06cce034/releases/6757038a191b26897c86e1e7/img/instagram-logo-240--1--1.png" alt="instagram-logo-240 (1) 1"></a>
+                </div><a href="https://maps.app.goo.gl/kyZ4KCZxv6tiEQnc6" target="_blank">
+                    <div class="button-6TkI5r button" data-id="105:66">
+                        <div class="rectangle-5-EDxDPY rectangle-5" data-id="105:67"></div>
+                        <div class="group-1-EDxDPY group-1" data-id="105:68">
+                            <div class="alamat-kami-ULY1Sr inter-medium-white-16px" data-id="105:69">Alamat Kami</div>
+                        </div>
+                    </div>
+                </a>
+            </footer>
+            <div class="bd-container-dLgTfx" data-id="25:247">
+                <div class="rectangle-1-zDhwfA" data-id="25:248"></div>
+                <div class="rectangle-2-zDhwfA" data-id="25:249"></div>
+                <div class="rectangle-3-zDhwfA rectangle-3" data-id="25:250"></div><a href="#services">
+                    <div class="layanan-zDhwfA layanan poppins-medium-dove-gray-16px" data-id="389:2">Layanan</div>
+                </a><a href="#ulasan">
+                    <div class="ulasan-zDhwfA ulasan poppins-medium-dove-gray-16px" data-id="243:4">Ulasan</div>
+                </a>
+                <div class="aku-bisa-zDhwfA aku-bisa-1 poppins-bold-blueberry-16px" data-id="25:251">Aku Bisa</div><a href="utama.php?page=list">
+                    <div class="beranda-zDhwfA poppins-medium-dove-gray-16px" data-id="25:253">Beranda</div>
+                </a><a href="#about">
+                    <div class="tentang-kami-zDhwfA tentang-kami poppins-medium-dove-gray-16px" data-id="25:254">Tentang kami</div>
+                </a><a href="utama.php?page=terapis">
+                    <div class="terapis-zDhwfA poppins-medium-dove-gray-16px" data-id="25:255">Terapis</div>
+                </a><a href="#menu">
+                    <div class="foto-kegiatan-zDhwfA poppins-medium-dove-gray-16px" data-id="25:256">Foto Kegiatan</div>
+                </a><a href="#contact">
+                    <div class="kontak-zDhwfA poppins-medium-dove-gray-16px" data-id="25:257">Kontak</div>
+                </a>
+                <div class="button-zDhwfA button" data-id="268:2"><a href="utama.php?page=login">
+                        <div class="login-cRrjoS valign-text-middle worksans-medium-white-15px" data-id="268:3">Login</div>
+                </a>
+            </div>
+            </div>
+        </div>
+    </div>
+    <script src="launchpad-js/launchpad-banner.js" async></script>
+    <script defer src="https://animaapp.s3.amazonaws.com/static/restart-btn.min.js"></script>
+</body>
+
+</html>
